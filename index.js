7c2deece120b0a3ce37159b9c36f40d7a627f0db
Ricky Bacon <rickyb@criticalmass.com>
2020-10-25 01:10:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+/*** index.js ***/
+'use strict';
+
+module.exports = {
+	epsilon: require('epsilon'),
+	delta: require('delta'),
+	theta: require('theta'),
+	alpha: require('alpha'),
+	alphaTheta: require('alpha-theta'),
+	betaLow: require('beta-low'),
+	betaHigh: require('beta-high'),
+	gamma: require('gamma'),
+	hyperGamma: require('hyper-gamma'),
+	lambda: require('lambda'),
+};
